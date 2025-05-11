63dea0afb152b1be395a2c663bcb1ab291e62cdf
Malik-Usman10 <xeteninit@MUsman>
2025-05-12 02:46:24 +0500

    Repository + Repositories Page Created

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ToDo
 1. Put Eye Icon In the passsword section for toggle functionality
 2. Make the Toast (Alerts) beautiful
-3. Make Dashboard and setup routes for sign in user 
+3. Make the Setting & Remaining UI
+4. Set the repository Page
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "home#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get '/dashboard', to: 'dashboard#index'
+  authenticated :user do
+    root to: 'dashboard#index', as: :dashboard
+    get 'dashboard/repositories', to: 'dashboard#repositories', as: :repositories
+    get 'dashboard/starred', to: 'dashboard#starred', as: :starred
+    get 'dashboard/explore', to: 'dashboard#explore', as: :explore
+    get 'dashboard/settings', to: 'dashboard#settings', as: :settings
+    get 'dashboard/repository', to: 'dashboard#repository', as: :repository
+  end
+
+  unauthenticated do
+    root to: "home#index", as: :unauthenticated_root
+  end
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
